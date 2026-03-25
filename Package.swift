720e5eb67a784cefba45920534ff7c9740ac59d6
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-03-25 13:14:22 +0000

    Release 5.7.0 - SDK 8.4.6

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "FyberAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/fyber-adapter/5.6.0/ISFyberAdapter5.6.0.zip",
-      checksum: "358f3a53a08a46c5a3f28fbedaf386b928fab38ade845468f8abcba0a5e08bf7"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/fyber-adapter/5.7.0/ISFyberAdapter5.7.0.zip",
+      checksum: "36b0cb5426081035f88793ec311c51cecabcfd7ee72bb636303ef9aa9a505047"
     )
   ]
 )
