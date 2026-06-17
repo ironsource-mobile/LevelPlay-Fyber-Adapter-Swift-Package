d928efb76644d962819cf4638400e10b75dfe33a
Gal Yedidovich <gal.yedidovich@unity3d.com>
2026-06-17 13:54:49 +0300

    Point mediation dependency to LevelPlay-Swift-Package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/inner-active/DTExchangeSDK-iOS-SPM", exact: "8.4.7"),
-    .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.0.0"..<"10.0.0"),
+    .package(url: "https://github.com/ironsource-mobile/LevelPlay-Swift-Package", "9.0.0"..<"10.0.0"),
   ],
   targets: [
     .target(
@@ -17,7 +17,7 @@ let package = Package(
       dependencies: [
         "FyberAdapterSDK",
         .product(name: "DTExchangeSDK", package: "DTExchangeSDK-iOS-SPM"),
-        .product(name: "UnityMediationSDK", package: "Unity-Mediation-iAds-Swift-Package"),
+        .product(name: "UnityMediationSDK", package: "LevelPlay-Swift-Package"),
       ]
     ),
     .binaryTarget(
